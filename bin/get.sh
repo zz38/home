4912c0390f9230dc88e8fd7f1fedba344c42b30e
技术总奸 <13768324554@139.com>
2018-10-04 19:39:17 +0800

    create

---
+++ bin/get.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ git submodule add https://github.com/zz38/dev.git   ../dev
+ git submodule add https://github.com/zz38/b.git     ../b
+ git submodule add https://github.com/zz38/git.git   ../git
+ git submodule add https://github.com/zz38/source.git  ../source
+ git submodule add https://github.com/zz38/idea.git   ../idea
